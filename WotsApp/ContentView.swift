5a3c52efad5446d917c0fd83d8254a135905b185
Mark Lucking <mark.lucking@gmail.com>
2020-02-29 18:11:56 +0100

    Initial Commit

---
+++ WotsApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WotsApp
+//
+//  Created by localadmin on 29.02.20.
+//  Copyright © 2020 Mark Lucking. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
